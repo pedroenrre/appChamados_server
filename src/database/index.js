94af3840fc3111975ac21de5d8d9fbfb31fdec84
pedroenrre <pedro_enrre@hotmail.com>
2020-12-18 17:56:16 -0300

    adds migration for creating department

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import Department from '../app/models/Department';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, Department];
 
 class Database {
   constructor() {
